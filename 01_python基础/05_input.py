710d8828607ed128dfded662a4cb3dcbf34a7422
hzy <1257664828@qq.com>
2021-05-30 22:35:41 +0800

    05_input

---
+++ 01_python基础/05_input.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+
+name = input('你的名字是:')
+birthday = input('你的生日是:')
+link_fruit = input('你喜欢的水果是：')
+link_sport = input('你喜欢的运动是：')
+
+print('你的名字是: %s,出生于: %s,你喜欢的水果是：%s,你喜欢的运动时：%s' % (name, birthday, link_fruit, link_sport))
