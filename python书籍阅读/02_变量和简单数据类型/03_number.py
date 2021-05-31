1405ac62236c3ddf582107027c652b9fc93c05f4
hzy <1257664828@qq.com>
2021-05-31 10:57:30 +0800

    数字

---
+++ python书籍阅读/02_变量和简单数据类型/03_number.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+
+# 数字
+# 1. 整数
+numbers1 = 1 + 3
+print(numbers1)
+numbers2 = 3 - 2
+print(numbers2)
+numbers3 = 2 * 3
+print(numbers3)
+numbers4 = 3 / 2
+print(numbers4)
+'''
+4
+1
+6
+1.5
+'''
+
+# 乘方运算
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+
+# 运算次序
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+# 2.浮点数
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(0.1 + 0.2)
+print(3 * 0.1)
+
+# 3. 使用函数str()避免错误
+age = 23
+message = 'Happy ' + str(age) + 'rd Birthday!'
+print(message)
