700619cd7dfd1a3886910f32a6e1c8d22234b4e0
hzy <1257664828@qq.com>
2021-05-31 10:34:17 +0800

    变量

---
+++ python书籍阅读/02_变量和简单数据类型/01_first_test.py
@@ -0,0 +1,8 @@
+message = 'hello,myName is codeHzy,I am coming learn python'
+print(message)
+
+messageTwo = 'hello,myName is codeHzy,I am coming learn python'
+print(messageTwo)
+messageTwo = 'hello python'
+print(messageTwo)
+
---
+++ python书籍阅读/02_变量和简单数据类型/01_varible.py
@@ -0,0 +1,19 @@
+message = 'hello world'
+print(message)
+
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+'''
+    命名规范
+    1、变量名只能包含字母、数字和下划线。变量名可以字母或者下划线大头，但是不能数字打头。
+    2、变量名不能包含空格，但可是用下划线来分隔其中单词。
+    3、不要将Python关键字和函数名用做变量名，即不要使用Python保留特殊用途的单词。
+    4、变量名应短精
+    5、使用小写字母l和大写字母O，容易看错
+    6、目前而言。应该小写的Python变量名。在变量命中使用大写字母虽然不会导致错误，但是避免使用了大写字母。
+'''
